59d05679cc991061338e1f845d6f3f07e5dcfcad
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2024-06-20 17:08:22 +0800

    Repairing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 require('./config/db');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
