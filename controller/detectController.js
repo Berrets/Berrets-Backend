cd01465e644917a53d4bd6c1699d1544237c6158
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2024-06-20 23:58:03 +0800

    Repairing

--- controller/detectController.js
+++ controller/detectController.js
@@ -2,14 +2,9 @@ const DetectModel = require("../models/DetectModel");
 
 module.exports = {
   detectData: async (req, res) => {
-    const { fileName, photo } = req.body;
+    const detectModel = new DetectModel(req.body);
     try {
-      const newDetect = new DetectModel({
-        fileName,
-        photo: Buffer.from(photo, 'base64')
-      });
-  
-      await newDetect.save();
+      const response = await detectModel.save();
       return res.status(201).json({ message: 'Success', data: response });
     } catch (error) {
       return res.status(500).json({ message: 'Error', err });
