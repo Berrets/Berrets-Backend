8d0381e3eac3085646d36bb241075840a680450b
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2024-06-20 18:47:39 +0800

    Repairing

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 require('./config/db');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
+
+app.get("/", (req, res) => {
+  console.log("Response success")
+  res.send("Response success!")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on PORT: ${PORT}`);
