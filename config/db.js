30c6fa9dea2ae405c44eaca920945b5dd295a630
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2024-06-20 16:38:20 +0800

    Repairing

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const url = process.env.MONGO_URL;
 
-mongoose.connect(url)
+mongoose.connect("mongodb+srv://berrets:VbF4B2JtKhv2YsBx@cluster0.pqae41d.mongodb.net/berrets-db?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log('MongoDB Connected...');
   }).catch((err) => {
